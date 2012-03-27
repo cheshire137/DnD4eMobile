510b14356dcc04d358ca3f52f876cd6d0a746d60
Sarah Vessels <cheshire137@gmail.com>
2012-03-26 20:38:16 -0400

    Applying custom theme.  Adding armor class, fortitude, etc. Highlighting positive modifiers in green, negative in red.  Making use of jQuery Mobile grid layouts instead of custom horizontal fieldsets. Adding controller.  Removing jQuery and jQuery Mobile files and instead pointing to online versions.  Downgrading jQuery because slider was not working with jQuery Mobile 1; known bug.  Fixing transitions between the two form pages.

---
+++ app.js
@@ -0,0 +1,54 @@
+var dnd = dnd || {};
+
+dnd.controller = (function($, document) {
+    var highlightPlusMinusInputs = function() {
+        console.log("Highlighting");
+        var handler = function() {
+            var input = $(this);
+            var value = Number(input.val());
+            if (value < 0) {
+                input.addClass('negative');
+                input.removeClass('positive');
+            } else if (0 == value) {
+                input.removeClass('negative');
+                input.removeClass('positive');
+            } else {
+                input.removeClass('negative');
+                input.addClass('positive');
+            }
+        };
+        $('input.plus-minus').change(handler);
+    };
+
+    var onPageChange = function(event, data) {
+        var toPageId = data.toPage.attr("id");
+        var fromPageId = null;
+        if (data.options.fromPage) {
+            fromPageId = data.options.fromPage.attr("id");
+        }
+        switch (toPageId) {
+        }
+    };
+
+    var onPageBeforeChange = function(event, data) {
+        if (typeof data.toPage === "string") {
+            var url = $.mobile.path.parseUrl(data.toPage);
+            if ($.mobile.path.isEmbeddedPage(url)) {
+                data.options.queryString = $.mobile.path.parseUrl(url.hash.replace(/^#/, "")).search.replace("?", "");
+            }
+        }
+    };
+
+    var init = function() {
+        console.log("Initializing controller");
+        var d = $(document);
+        d.bind("pagebeforechange", onPageBeforeChange);
+        d.bind("pagechange", onPageChange);
+        d.bind("pageinit", highlightPlusMinusInputs);
+    };
+
+    return {
+        init: init
+    };
+})(jQuery, document);
+
